a4fc93bd0ba600114dd2f692183ba277c3e5e458
Kevin Stradtman <stradtkt@gmail.com>
2018-11-17 18:27:52 -0500

    Added the ability to use North instead of just n and so on

--- src/com/collections/adventure/Main.java
+++ src/com/collections/adventure/Main.java
@@ -18,14 +18,55 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+        Map<String, String> vocab = new HashMap<>();
+        vocab.put("QUIT", "Q");
+        vocab.put("NORTH", "N");
+        vocab.put("EAST", "E");
+        vocab.put("SOUTH", "S");
+        vocab.put("WEST", "W");
+
         int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDescription());
+
             if(loc == 0) {
                 break;
             }
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)) {
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are");
+            for(String exit: exits.keySet()) {
+                System.out.println(exit + ",");
+            }
+            String direction = scanner.nextLine().toUpperCase();
+
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words) {
+                    if(vocab.containsKey(word)) {
+                        direction = vocab.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
