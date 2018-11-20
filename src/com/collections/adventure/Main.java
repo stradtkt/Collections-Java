8e909fa601c6054036333fc4e05819713b2ca8a3
Kevin Stradtman <stradtkt@gmail.com>
2018-11-20 09:17:47 -0500

    Added a shopping basket

--- src/com/collections/adventure/Main.java
+++ src/com/collections/adventure/Main.java
@@ -10,28 +10,34 @@ public class Main {
     private static Map<Integer, Location> locations = new HashMap<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> tempExit = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of your computer learning Java", tempExit));
 
-        locations.put(0, new Location(0, "You are sitting in front of your computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of the hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        tempExit = new HashMap<>();
+        tempExit.put("W", 2);
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        tempExit = new HashMap<>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of the hill", tempExit));
 
-        locations.get(2).addExit("N", 5);
+        tempExit = new HashMap<>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
 
-        locations.get(3).addExit("W", 1);
+        tempExit = new HashMap<>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        tempExit = new HashMap<>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest", tempExit));
 
         Map<String, String> vocab = new HashMap<>();
         vocab.put("QUIT", "Q");
@@ -50,7 +56,7 @@ public class Main {
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.println("Available exits are");
             for(String exit: exits.keySet()) {
-                System.out.println(exit + ",");
+                System.out.println(exit + ", ");
             }
             String direction = scanner.nextLine().toUpperCase();
 
---
+++ src/com/collections/store/Main.java
@@ -0,0 +1,116 @@
+package com.collections.store;
+
+import java.util.Map;
+
+public class Main {
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 0.86, 100);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cake", 1.10, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.50, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.50, 200);
+        stockList.addStock(temp);
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.50, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        for(String s: stockList.Items().keySet()) {
+            System.out.println(s);
+        }
+
+        Basket kevinsBasket = new Basket("Kevin");
+        sellItem(kevinsBasket, "car", 1);
+        System.out.println(kevinsBasket);
+
+        sellItem(kevinsBasket, "car", 1);
+        System.out.println(kevinsBasket);
+
+        if(sellItem(kevinsBasket, "car", 1) != 1) {
+            System.out.println("There are no more cars in stock");
+        }
+
+        sellItem(kevinsBasket, "spanner", 5);
+        System.out.println(kevinsBasket);
+
+        sellItem(kevinsBasket, "juice", 4);
+        sellItem(kevinsBasket, "cup", 12);
+        sellItem(kevinsBasket, "bread", 1);
+        System.out.println(kevinsBasket);
+
+        System.out.println(stockList);
+
+        Basket basket = new Basket("customer");
+        sellItem(basket, "car", 1);
+        sellItem(basket, "keys", 2);
+        sellItem(basket, "armoral", 20);
+        removeItem(basket, "armoral", 5);
+        System.out.println(basket);
+        System.out.println(stockList);
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+        stockList.Items().get("car").adjustStock(2000);
+        stockList.Items().get("car").adjustStock(-1000);
+        System.out.println(stockList);
+
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(stockList.reserveStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return basket.addToBasket(stockItem, quantity);
+        }
+        return 0;
+    }
+
+    public static int removeItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(basket.removeFromBasket(stockItem, quantity) == quantity) {
+            return stockList.unreserveStock(item, quantity);
+        }
+        return 0;
+    }
+    public static void checkout(Basket basket) {
+        for(Map.Entry<StockItem, Integer> item : basket.Items().entrySet()) {
+            stockList.sellStock(item.getKey().getName(), item.getValue());
+        }
+        basket.clearBasket();
+    }
+}
